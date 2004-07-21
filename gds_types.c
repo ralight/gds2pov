4dcc6bc19361b70d22488e104d996567c3695942
roger <devnull@localhost>
2004-07-21 23:07:18 +0100

    [svn] Made Linux compatible

--- gds_types.c
+++ gds_types.c
@@ -5,25 +5,39 @@
 #include <string.h>
 #include <math.h>
 
+#ifdef LINUX
+#include <byteswap.h>
+#endif
+
 #include "gds2pov.h"
 
 long endian_swap_long(long value)
 {
+#ifdef WIN32
 	_asm{
 		mov eax,value
 		bswap eax
 		mov value,eax
 	}
+#endif
+#ifdef LINUX
+	value = bswap_32(value);
+#endif
 	return value;
 }
 
 short endian_swap_short(short value)
 {
+#ifdef WIN32
 	_asm{
 		mov ax,value
 		xchg ah,al
 		mov value,ax
 	}
+#endif
+#ifdef LINUX
+	value = bswap_16(value);
+#endif
 	return value;
 }
 
