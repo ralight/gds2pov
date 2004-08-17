fc82ee5065d44c442765c4885e8d22486f8559af
roger <devnull@localhost>
2004-08-17 12:14:31 +0100

    [svn] Bumped version number.
    Started adding -v (verbose) option. This needs to be implemented and also the command line parsing needs improving desperately.
    
    Default font used is now crystal.ttf which is supplied with POV-Ray.
    
    Updated example_config.txt to show how a font will be able to be specified in the next release.

--- gds2pov.cpp
+++ gds2pov.cpp
@@ -6,7 +6,7 @@
 #include "gds_globals.h"
 #include "process_cfg.h"
 
-#define VERSION 0.5
+#define VERSION 0.6
 
 void printusage()
 {
@@ -14,14 +14,16 @@ void printusage()
 		printf("Copyright (C) 2004 by Roger Light\nhttp://www.atchoo.org/gds2pov/\n\n");
 		printf("gds2pov comes with ABSOLUTELY NO WARRANTY.  You may distribute gds2pov freely as described in the readme.txt distributed with this file.\n\n");
 		printf("gds2pov is a program for converting a GDS2 file to a POV-Ray scene file.\n\n");
-		printf("Usage: gds2pov input.gds output.pov [-c config.txt] [-p process.txt]\n\n");
-		printf("Options\n -c\t\tSpecify config file\n -p\t\tSpecify process file\n\n");
+		printf("Usage: gds2pov input.gds output.pov [-c config.txt] [-p process.txt] [-v]\n\n");
+		printf("Options\n -c\t\tSpecify config file\n -p\t\tSpecify process file\n -v\t\tVerbose output\n\n");
 		printf("See http://www.atchoo.org/gds2pov/ for updates.\n");
 }
 
 int main(int argc, char *argv[])
 {
-	if(argc<3 || argc==4 || argc==6 || argc>7){
+	bool verbose=false;
+
+	if(argc<3 || argc==4 || argc==6 || argc>8){
 		printusage();
 		return 0;
 	}
@@ -33,7 +35,10 @@ int main(int argc, char *argv[])
 	char *processfile=NULL;
 
 	if(argc>4){
-		if(strncmp(argv[3], "-c",strlen("-c"))!=0 && strncmp(argv[3], "-p",strlen("-p"))!=0){
+		if(strncmp(argv[3], "-c",strlen("-c"))!=0 
+				&& strncmp(argv[3], "-p",strlen("-p"))!=0
+				&& strncmp(argv[3], "-v",strlen("-v"))!=0
+				){
 			printusage();
 			return 0;
 		}else{
@@ -41,12 +46,15 @@ int main(int argc, char *argv[])
 				configfile = argv[4];
 			}else if(strncmp(argv[3], "-p", strlen("-p"))==0){
 				processfile = argv[4];
+			}else if(strncmp(argv[3], "-v", strlen("-v"))==0 || strncmp(argv[3], "-v", strlen("-v"))==0){
+				verbose = true;
 			}else{
 				printusage();
 			}
 
 		}
 	}
+	// FIXME - need to consider just >5 arguments (so (-c or -p ) and -v)
 	if(argc>6){
 		if( (strncmp(argv[5], "-c",strlen("-c"))!=0 && strncmp(argv[5], "-p",strlen("-p"))!=0) || strncmp(argv[3], argv[5], strlen(argv[3]))==0){
 			printusage();
@@ -56,6 +64,8 @@ int main(int argc, char *argv[])
 				configfile = argv[6];
 			}else if(strncmp(argv[5], "-p", strlen("-p"))==0){
 				processfile = argv[6];
+			}else if(strncmp(argv[5], "-v", strlen("-v"))==0 || strncmp(argv[6], "-v", strlen("-v"))==0){
+				verbose = true;
 			}else{
 				printusage();
 			}
