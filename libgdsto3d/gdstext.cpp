abc051eec0033e985b0bac12004adebebaaf5c18
roger <devnull@localhost>
2005-05-21 11:17:15 +0100

    [svn] Added license information to all finished or nearly finished files.
    Added more todo item!

--- libgdsto3d/gdstext.cpp
+++ libgdsto3d/gdstext.cpp
@@ -1,3 +1,25 @@
+/*
+ * File: gdstext.cpp
+ * Author: Roger Light
+ * Project: gdsto3d
+ *
+ * This is the GDSText class which is used to represent the GDS text object.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
 #include "gdsobject.h"
 #include "gdstext.h"
 
