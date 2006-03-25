6462da1e45376daca8156dcdbdcc779dac0e4db6
roger <devnull@localhost>
2006-03-25 18:20:22 +0000

    [svn] Added start of gds2info. It doesn't work - needs debugging.

---
+++ gds2info/gdsobject_info.h
@@ -0,0 +1,42 @@
+/*
+ * File: gdsobject_info.h
+ * Author: Roger Light
+ * Project: gdsto3d
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifndef __GDSOBJECT_INFO_H__
+#define __GDSOBJECT_INFO_H__
+
+#include "gdsobject.h"
+
+class GDSObject_info : public GDSObject
+{
+public:
+	GDSObject_info(char *Name);
+	~GDSObject_info();
+
+	virtual void OutputToFile(FILE *fptr, class GDSObjects *Objects, char *Font, float offx, float offy, long *objectid, struct ProcessLayer *firstlayer);
+	void OutputPathToFile(FILE *fptr, class GDSObjects *Objects, char *Font, float offx, float offy, long *objectid, struct ProcessLayer *firstlayer);
+	void OutputPolygonToFile(FILE *fptr, class GDSObjects *Objects, char *Font, float offx, float offy, long *objectid, struct ProcessLayer *firstlayer);
+	void OutputTextToFile(FILE *fptr, class GDSObjects *Objects, char *Font, float offx, float offy, long *objectid, struct ProcessLayer *firstlayer);
+	void OutputSRefToFile(FILE *fptr, class GDSObjects *Objects, char *Font, float offx, float offy, long *objectid, struct ProcessLayer *firstlayer);
+	void OutputARefToFile(FILE *fptr, class GDSObjects *Objects, char *Font, float offx, float offy, long *objectid, struct ProcessLayer *firstlayer);
+
+};
+
+#endif // __GDSOBJECT_INFO_H__
+
