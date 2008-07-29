79a1f96369aaf92908d4fdfb242f94a7cab7f792
Roger Light <roger@atchoo.org>
2008-07-29 14:03:53 +0100

    Escape % properly.

--- gds2svg/gdsparse_svg.cpp
+++ gds2svg/gdsparse_svg.cpp
@@ -63,10 +63,10 @@ void GDSParse_svg::OutputFooter()
 		
 		fprintf(_optr, "\t</defs>\n");
 		if(_topcellname.length() > 0){
-			fprintf(_optr, "\t<use x=\"%.2f\" y=\"%.2f\" width=\"100%\" height=\"100%\" xlink:href=\"#%s\"/>\n", -_scale*Boundary->XMin/2, -_scale*Boundary->YMin/2, _topcellname.c_str());
+			fprintf(_optr, "\t<use x=\"%.2f\" y=\"%.2f\" width=\"100%%\" height=\"100%%\" xlink:href=\"#%s\"/>\n", -_scale*Boundary->XMin/2, -_scale*Boundary->YMin/2, _topcellname.c_str());
 		}else{
 			if(_Objects[0]){
-				fprintf(_optr, "\t<use x=\"%.2f\" y=\"%.2f\" width=\"100%\" height=\"100%\" xlink:href=\"#%s\"/>\n", -_scale*Boundary->XMin/2, -_scale*Boundary->YMin/2, _Objects[0]->GetName().c_str());
+				fprintf(_optr, "\t<use x=\"%.2f\" y=\"%.2f\" width=\"100%%\" height=\"100%%\" xlink:href=\"#%s\"/>\n", -_scale*Boundary->XMin/2, -_scale*Boundary->YMin/2, _Objects[0]->GetName().c_str());
 		}else{
 			}
 		}
