9750c12498e3f8df6d5c021df32407a30f88c09d
Roger Light <roger@atchoo.org>
2025-06-25 11:21:19 +0100

    Fix windows byte / std::byte incompatibility

--- libgdsto3d/gdsparse.h
+++ libgdsto3d/gdsparse.h
@@ -32,35 +32,6 @@ using namespace std;
 #include "gds_globals.h"
 #include "gdsobject.h"
 
-/*
- * We need byte swapping functions.
- * If these exist for a particular platform, use the (presumably) optimised
- * versions provided. Otherwise, use the network functions htonl() and htons().
- */
-
-#if defined(linux)
-#	include <endian.h>
-#	include <byteswap.h>
-#	define endian_swap_long(A) bswap_32((A))
-#	define endian_swap_short(A) bswap_16((A))
-#elif defined(WIN32)
-//#	define __LITTLE_ENDIAN 1234
-//#	define __BIG_ENDIAN 4321
-//#	define __BYTE_ORDER __LITTLE_ENDIAN
-//	long endian_swap_long(long value);
-//	short endian_swap_short(short value);
-#	include <winsock2.h>
-#	define endian_swap_long(A) htonl((A))
-#	define endian_swap_short(A) htons((A))
-#else
-	/* Generic includes for Unix-alike systems */
-	/* Includes at least Solaris, FreeBSD, HP-UX */
-#	include <arpa/nameser_compat.h>
-#	include <netinet/in.h>
-#	define endian_swap_long(A) htonl((A))
-#	define endian_swap_short(A) htons((A))
-#endif
-
 class GDSParse_pov;
 
 class GDSParse
--- libgdsto3d/gdsparse_basic_types.cpp
+++ libgdsto3d/gdsparse_basic_types.cpp
@@ -27,6 +27,37 @@
 
 #include "gdsparse.h"
 
+/*
+ * We need byte swapping functions.
+ * If these exist for a particular platform, use the (presumably) optimised
+ * versions provided. Otherwise, use the network functions htonl() and htons().
+ */
+
+#if defined(linux)
+#	include <endian.h>
+#	include <byteswap.h>
+#	define endian_swap_long(A) bswap_32((A))
+#	define endian_swap_short(A) bswap_16((A))
+#elif defined(WIN32)
+//#	define __LITTLE_ENDIAN 1234
+//#	define __BIG_ENDIAN 4321
+//#	define __BYTE_ORDER __LITTLE_ENDIAN
+//	long endian_swap_long(long value);
+//	short endian_swap_short(short value);
+#   define byte win_byte_override
+#	include <winsock2.h>
+#   undef byte
+#	define endian_swap_long(A) htonl((A))
+#	define endian_swap_short(A) htons((A))
+#else
+	/* Generic includes for Unix-alike systems */
+	/* Includes at least Solaris, FreeBSD, HP-UX */
+#	include <arpa/nameser_compat.h>
+#	include <netinet/in.h>
+#	define endian_swap_long(A) htonl((A))
+#	define endian_swap_short(A) htons((A))
+#endif
+
 
 short GDSParse::GetBitArray()
 {
